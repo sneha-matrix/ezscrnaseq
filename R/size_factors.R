79c47d1a23190f334a29fdd5abd394d88bb80f74
jonathan dreyfuss <jdreyf@gmail.com>
2019-10-29 19:47:01 -0400

    update size_factors

--- R/size_factors.R
+++ R/size_factors.R
@@ -34,29 +34,27 @@ size_factors <- function(sce, min.size=200, max.size=3000, min.mean=0.1, group.c
 
 
   #rm zero size factors
-  if(any(sizeFactors(sce)==0)){
+  if (any(sizeFactors(sce)==0)){
     if (verbose) cat("\nRemoveing", sum(sizeFactors(sce)==0), "cells that have size factor of zero.\n")
     sce <- sce[, sizeFactors(sce) > 0]
   }
 
   # scatter
   if (plot){
-
     if (is.null(sce$total_counts)) stop("Total counts are not calculated yet.")
 
     grDevices::pdf(paste(c(prefix, "size_factor_scatter.pdf"), collapse="_"))
     on.exit(grDevices::dev.off())
 
-    if(!is.null(group.col)){
-      plot(sizeFactors(sce), sce$total_counts/1e3, log="xy", ylab="Library size (thousands)", xlab="Size factor", main="Size factors from deconvolution",
-           col= scales::alpha(as.numeric(as.factor(colData(sce)[, group.col]))+1, 0.3), pch=16)
+    if (!is.null(group.col)){
+      graphics::plot(sizeFactors(sce), sce$total_counts/1e3, log="xy", ylab="Library size (thousands)", xlab="Size factor", main="Size factors from deconvolution",
+           col=scales::alpha(as.numeric(as.factor(colData(sce)[, group.col]))+1, 0.3), pch=16)
       legd <- sort(unique(colData(sce)[, group.col]))
       graphics::legend("topleft", col=2:(length(legd)+1), pch=16, cex=1.2, legend=legd)
-    }else{
-      plot(sizeFactors(sce), sce$total_counts/1e3, log="xy", ylab="Library size (thousands)", xlab="Size factor", main="Size factors from deconvolution",
+    } else {
+      graphics::plot(sizeFactors(sce), sce$total_counts/1e3, log="xy", ylab="Library size (thousands)", xlab="Size factor", main="Size factors from deconvolution",
            col= scales::alpha(1, 0.3), pch=16)
     }
   }
-
   return(sce)
 }

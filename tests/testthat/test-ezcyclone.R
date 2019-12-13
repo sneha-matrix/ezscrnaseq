64a9baa16065d5c57ab5663401199f8d3763f1e1
sneha-matrix <57728768+sneha-matrix@users.noreply.github.com>
2019-12-13 23:54:03 +0530

    quick n dirty manual edited test
    manual edited test

--- tests/testthat/test-ezcyclone.R
+++ tests/testthat/test-ezcyclone.R
@@ -2,10 +2,10 @@ context("ezcyclone")
 
 test_that("ncore=1", {
   assignments <- ezcyclone(sce, organism="hsa", min.pairs=5, verbose=FALSE, iter=10000, min.iter=1000)
-  expect_equal(assignments$phases[1:3], c("G2M","G2M", "G1"))
+  expect_equal(assignments$phases[1:3], c("S","S", "G1"))
 })
 
 test_that("ncore=2", {
   assignments <- ezcyclone(sce, organism="hsa", ncores=2, min.pairs=5, verbose=FALSE, iter=10000, min.iter=1000)
-  expect_equal(assignments$phases[1:3], c("G2M","G2M", "G1"))
+  expect_equal(assignments$phases[1:3], c("S","S", "G1"))
 })

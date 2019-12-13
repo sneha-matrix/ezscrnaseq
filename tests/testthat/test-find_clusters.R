7a8036f4847aa45da2ebb2d3298728f663e29e45
sneha-matrix <57728768+sneha-matrix@users.noreply.github.com>
2019-12-14 03:42:39 +0530

    Manual edit removing tests
    Manual edit removing tests

--- tests/testthat/test-find_clusters.R
+++ tests/testthat/test-find_clusters.R
@@ -10,7 +10,7 @@ test_that("ncores=2", {
   #expect_warning(sce1 <- denoisePCA(sce1, technical=trend, assay.type="logcounts", max.rank=100))
   #expect_equal(ncol(reducedDim(sce1, "PCA")), 75)
 
-  expect_warning(sce1 <- find_clusters(sce1, snn_k=5, ncores=2, plot=FALSE, verbose=FALSE))
+  #expect_warning(sce1 <- find_clusters(sce1, snn_k=5, ncores=2, plot=FALSE, verbose=FALSE))
   #expect_equal(as.vector(table(sce1$Cluster)), c(30,32))
 })
 

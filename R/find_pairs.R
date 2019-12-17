5586971c5677323bb36691384ffda27d169136d4
jonathan dreyfuss <jdreyf@gmail.com>
2019-12-17 12:17:42 -0500

    update find_pair docs

--- R/find_pairs.R
+++ R/find_pairs.R
@@ -3,17 +3,18 @@
 #' Cell cycle pairs creation using \pkg{scran} \code{sandbag}.
 #'
 #' @param genes Ensembl gene IDs for genes in \code{sce}.
-#' @param G1 vector of column number having G1 genes expression
-#' @param S vector of column number having S genes expression
-#' @param G2M vector of column number having G2M genes expression
+#' @param G1 vector of column indices of G1 genes.
+#' @param S vector of column indices of S genes.
+#' @param G2M vector of column indices of G2M genes.
 #' @inheritParams qc_metrics
 #' @inheritParams scran::sandbag
+#' @details Genes in all three phases are required.
 #' @return A list containing training gene pairs for G1, S, and G2M phase.
 #' @export
 
 find_pairs <- function(sce, G1, S, G2M, genes=NULL){
   if (is.null(genes)) genes <- rownames(sce)
-  stopifnot(!is.null(G1), !is.null(S), !is.null(G2M), !is.null(rownames(sce)),
+  stopifnot(is.numeric(G1), is.numeric(G2M), is.numeric(S), !is.null(rownames(sce)),
             length(which(rownames(sce) %in% genes)) > 0)
 
   sce2 <- sce[which(rownames(sce) %in% genes),, drop=FALSE]
